be0a7de89346346ed9ea76ca9d8de1feb2f8588b
MertSaygili <mert71719601@gmail.com>
2023-11-25 16:32:28 +0300

    init

---
+++ src/main/java/hobby_garden/hobby_garden_server/HobbyGardenServerApplication.java
@@ -0,0 +1,13 @@
+package hobby_garden.hobby_garden_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyGardenServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbyGardenServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hobby_garden/hobby_garden_server/HobbyGardenServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hobby_garden.hobby_garden_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyGardenServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
