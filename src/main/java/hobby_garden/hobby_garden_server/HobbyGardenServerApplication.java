e84768eb53238086b6877ec6ebb143febe95124b
MertSaygili <mert71719601@gmail.com>
2023-12-18 23:03:49 +0300

    fix

--- src/main/java/hobby_garden/hobby_garden_server/HobbyGardenServerApplication.java
+++ src/main/java/hobby_garden/hobby_garden_server/HobbyGardenServerApplication.java
@@ -3,8 +3,6 @@ package hobby_garden.hobby_garden_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// swagger path = http://localhost:8080/swagger-ui/index.html
-
 @SpringBootApplication
 public class HobbyGardenServerApplication {
 	public static void main(String[] args) {
