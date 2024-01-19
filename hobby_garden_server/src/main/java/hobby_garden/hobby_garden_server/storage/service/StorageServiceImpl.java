f64c840f6553c921ae2f4f520e02eb76ec9ab9c7
MertSaygili <mert71719601@gmail.com>
2024-01-20 02:57:03 +0300

    feature: filter exception-- storage service

--- hobby_garden_server/src/main/java/hobby_garden/hobby_garden_server/storage/service/StorageServiceImpl.java
+++ hobby_garden_server/src/main/java/hobby_garden/hobby_garden_server/storage/service/StorageServiceImpl.java
@@ -2,6 +2,7 @@ package hobby_garden.hobby_garden_server.storage.service;
 
 import hobby_garden.hobby_garden_server.common.constants.Strings;
 import hobby_garden.hobby_garden_server.common.dto.BaseResponse;
+import hobby_garden.hobby_garden_server.common.exception.FilterExceptions;
 import hobby_garden.hobby_garden_server.common.exception.exceptions.UnknownException;
 import hobby_garden.hobby_garden_server.common.utility.ImageUtils;
 import hobby_garden.hobby_garden_server.post.model.Post;
@@ -20,6 +21,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Filter;
 
 @Service
 @RequiredArgsConstructor
@@ -32,28 +34,38 @@ public class StorageServiceImpl implements StorageService {
 
     @Override
     public BaseResponse<?> uploadImageToPost(String token, String postId, MultipartFile imageBase64) {
-        //* check user
-        getUserByToken(token);
+        try {
+            //* check user
+            getUserByToken(token);
 
-        System.out.println(token);
+            System.out.println(token);
 
-        Post post = postRepository.findById(postId).orElseThrow(() -> new UnknownException(Strings.postNotFound));
+            Post post = postRepository.findById(postId).orElseThrow(() -> new UnknownException(Strings.postNotFound));
 
-        List<Image> images  = post.getImages();
-        images.add(uploadImage(imageBase64));
-        post.setImages(images);
+            List<Image> images = post.getImages();
+            images.add(uploadImage(imageBase64));
+            post.setImages(images);
 
-        postRepository.save(post);
+            postRepository.save(post);
 
-        return new BaseResponse<>(true, Strings.imageUploadedSuccessfully, null);
+            return new BaseResponse<>(true, Strings.imageUploadedSuccessfully, null);
+        }
+        catch (Exception e) {
+            throw new FilterExceptions(e.getMessage());
+        }
     }
 
     @Override
     public ResponseEntity<?> getImage(String token, long fileId) {
-        //* check user
-        getUserByToken(token);
+        try {
+            //* check user
+            getUserByToken(token);
 
-        return ResponseEntity.ok().contentType(MediaType.valueOf("image/png")).body(downloadImage(fileId));
+            return ResponseEntity.ok().contentType(MediaType.valueOf("image/png")).body(downloadImage(fileId));
+        }
+        catch (Exception e) {
+            throw new FilterExceptions(e.getMessage());
+        }
     }
 
     private User getUserByToken(String token) {
