8308745ef9642eabbc8b4037f9f2fff7dc633263
MertSaygili <mert71719601@gmail.com>
2024-01-20 02:55:39 +0300

    feature: filter exception-- user service

--- hobby_garden_server/src/main/java/hobby_garden/hobby_garden_server/user/service/UserServiceImpl.java
+++ hobby_garden_server/src/main/java/hobby_garden/hobby_garden_server/user/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package hobby_garden.hobby_garden_server.user.service;
 import hobby_garden.hobby_garden_server.common.constants.Strings;
 import hobby_garden.hobby_garden_server.common.dto.BaseResponse;
 import hobby_garden.hobby_garden_server.common.enums.Roles;
+import hobby_garden.hobby_garden_server.common.exception.FilterExceptions;
 import hobby_garden.hobby_garden_server.common.exception.exceptions.UnknownException;
 import hobby_garden.hobby_garden_server.common.exception.exceptions.UserAlreadyExist;
 import hobby_garden.hobby_garden_server.hobby.model.Hobby;
@@ -35,112 +36,130 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public BaseResponse<SignInResponse> signIn(SignInRequest signInRequest) {
-        //* check if user exist
-        if(userRepository.findByUsername(signInRequest.getUsername()).isEmpty()) {
-            return new BaseResponse<>(false, Strings.userNotFound, null);
+        try {
+            //* check if user exist
+            if (userRepository.findByUsername(signInRequest.getUsername()).isEmpty()) {
+                return new BaseResponse<>(false, Strings.userNotFound, null);
+            }
+
+            //* get user from db
+            User user = userRepository.findByUsername(signInRequest.getUsername()).get();
+
+            //* authenticate user, generate jwt and refresh token
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    signInRequest.getUsername(),
+                    signInRequest.getPassword()
+            ));
+            String jwt = jwtService.generateToken(user);
+            String refreshToken = jwtService.generateRefreshToken(new HashMap<>(), user);
+
+            //* create response body and return
+            SignInResponse response = new SignInResponse(
+                    jwt,
+                    user.getUserId(),
+                    refreshToken
+            );
+
+            return new BaseResponse<>(true, Strings.userSignedIn, response);
         }
-
-        //* get user from db
-        User user = userRepository.findByUsername(signInRequest.getUsername()).get();
-
-        //* authenticate user, generate jwt and refresh token
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                signInRequest.getUsername(),
-                signInRequest.getPassword()
-        ));
-        String jwt = jwtService.generateToken(user);
-        String refreshToken = jwtService.generateRefreshToken(new HashMap<>(), user);
-
-        //* create response body and return
-        SignInResponse response = new SignInResponse(
-                jwt,
-                user.getUserId(),
-                refreshToken
-        );
-
-        return new BaseResponse<>(true, Strings.userSignedIn, response);
+        catch (Exception e) {
+            throw new FilterExceptions(e.getMessage());
+        }
     }
 
     @Override
     public BaseResponse<Object> signUp(SignUpRequest signUpRequest) {
-        //* check if user already exist
-        if(userRepository.findByUsername(signUpRequest.getUsername()).isPresent()) {
-            throw new UserAlreadyExist(Strings.usernameInUse);
-        }
 
-        //* create new user
-        User user = new User();
-        user.setUsername(signUpRequest.getUsername());
-        user.setPassword(passwordMapper.encode(signUpRequest.getPassword()));
-        user.setFirstNameLastName(signUpRequest.getFirstNameLastName());
-        user.setEmail(signUpRequest.getEmail());
-        user.setCreatedAt(LocalDateTime.now());
-        user.setRole(Roles.USER);
-        user.setHobbies(new ArrayList<>(Collections.emptyList()));
-
-        //* user hobby relationship integration
-        List<Hobby> userHobbies = new ArrayList<>(Collections.emptyList());
-        for (String hobbyName : signUpRequest.getHobbies()) {
-            userHobbies.add(hobbyService.createHobbyToUser(hobbyName));
-        }
-        user.setHobbies(userHobbies);
-
-        //* save user to db
         try {
-            userRepository.save(user);
-            return new BaseResponse<>(true, Strings.userCreated, null);
-        } catch (Exception e) {
-            throw new UnknownException(Strings.unknownExceptionWhileCreatingUser + " " + e.getMessage());
+            //* check if user already exist
+            if (userRepository.findByUsername(signUpRequest.getUsername()).isPresent()) {
+                throw new UserAlreadyExist(Strings.usernameInUse);
+            }
+
+            //* create new user
+            User user = new User();
+            user.setUsername(signUpRequest.getUsername());
+            user.setPassword(passwordMapper.encode(signUpRequest.getPassword()));
+            user.setFirstNameLastName(signUpRequest.getFirstNameLastName());
+            user.setEmail(signUpRequest.getEmail());
+            user.setCreatedAt(LocalDateTime.now());
+            user.setRole(Roles.USER);
+            user.setHobbies(new ArrayList<>(Collections.emptyList()));
+
+            //* user hobby relationship integration
+            List<Hobby> userHobbies = new ArrayList<>(Collections.emptyList());
+            for (String hobbyName : signUpRequest.getHobbies()) {
+                userHobbies.add(hobbyService.createHobbyToUser(hobbyName));
+            }
+            user.setHobbies(userHobbies);
+
+            //* save user to db
+            try {
+                userRepository.save(user);
+                return new BaseResponse<>(true, Strings.userCreated, null);
+            } catch (Exception e) {
+                throw new UnknownException(Strings.unknownExceptionWhileCreatingUser + " " + e.getMessage());
+            }
+        }
+        catch (Exception e) {
+            throw new FilterExceptions(e.getMessage());
         }
     }
 
     @Override
     public BaseResponse<String> updatePassword(String token, UpdatePasswordRequest updatePasswordRequest) {
-        //* get user from db
-        User user = getUserNameByToken(token);
+        try {
+            //* get user from db
+            User user = getUserNameByToken(token);
 
-        //* check if old password is correct
-        if(passwordMapper.matches(updatePasswordRequest.getOldPassword(), user.getPassword())) {
-            user.setPassword(passwordMapper.encode(updatePasswordRequest.getNewPassword()));
-            userRepository.save(user);
-            try{
+            //* check if old password is correct
+            if (passwordMapper.matches(updatePasswordRequest.getOldPassword(), user.getPassword())) {
+                user.setPassword(passwordMapper.encode(updatePasswordRequest.getNewPassword()));
                 userRepository.save(user);
-                return new BaseResponse<>(true, Strings.userUpdated, null);
+                try {
+                    userRepository.save(user);
+                    return new BaseResponse<>(true, Strings.userUpdated, null);
 
-            } catch (Exception e) {
-                throw new UnknownException(Strings.unknownExceptionWhileUpdatingUser + " " + e.getMessage());
+                } catch (Exception e) {
+                    throw new UnknownException(Strings.unknownExceptionWhileUpdatingUser + " " + e.getMessage());
+                }
+            } else {
+                return new BaseResponse<>(false, Strings.wrongUserPassword, null);
             }
         }
-        else{
-            return new BaseResponse<>(false, Strings.wrongUserPassword, null);
+        catch (Exception e) {
+            throw new FilterExceptions(e.getMessage());
         }
     }
 
     @Override
     public BaseResponse<String> forgotPassword(ForgotPasswordRequest forgotPasswordRequest) {
-        Optional<User> user = userRepository.findByUsername(forgotPasswordRequest.getUsername());
+        try {
+            Optional<User> user = userRepository.findByUsername(forgotPasswordRequest.getUsername());
 
-        //* check user is existed or not, username is unique
-        if(user.isPresent()) {
-            User userInfo = user.get();
-            //* check email is correct, then send [email] with new password
-            if(userInfo.getEmail().equals(forgotPasswordRequest.getEmail())) {
-                userInfo.setPassword(passwordMapper.encode(forgotPasswordRequest.getNewPassword()));
-                userRepository.save(userInfo);
-                try{
+            //* check user is existed or not, username is unique
+            if (user.isPresent()) {
+                User userInfo = user.get();
+                //* check email is correct, then send [email] with new password
+                if (userInfo.getEmail().equals(forgotPasswordRequest.getEmail())) {
+                    userInfo.setPassword(passwordMapper.encode(forgotPasswordRequest.getNewPassword()));
                     userRepository.save(userInfo);
-                    return new BaseResponse<>(true, Strings.passwordChanged, null);
+                    try {
+                        userRepository.save(userInfo);
+                        return new BaseResponse<>(true, Strings.passwordChanged, null);
 
-                } catch (Exception e) {
-                    throw new UnknownException(Strings.unknownExceptionWhileUpdatingUser + " " + e.getMessage());
+                    } catch (Exception e) {
+                        throw new UnknownException(Strings.unknownExceptionWhileUpdatingUser + " " + e.getMessage());
+                    }
+                } else {
+                    return new BaseResponse<>(false, Strings.userNotFound, null);
                 }
-            }
-            else{
+            } else {
                 return new BaseResponse<>(false, Strings.userNotFound, null);
             }
         }
-        else{
-            return new BaseResponse<>(false, Strings.userNotFound, null);
+        catch (Exception e) {
+            throw new FilterExceptions(e.getMessage());
         }
 
     }
