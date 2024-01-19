dec9aa2aeefb756cab0aa8763bf8bccf4e990f74
MertSaygili <mert71719601@gmail.com>
2024-01-20 02:52:49 +0300

    feature: filter exception class added

--- hobby_garden_server/src/main/java/hobby_garden/hobby_garden_server/common/constants/Strings.java
+++ hobby_garden_server/src/main/java/hobby_garden/hobby_garden_server/common/constants/Strings.java
@@ -4,59 +4,54 @@ package hobby_garden.hobby_garden_server.common.constants;
 public  class Strings {
 
     //* User related strings
-    public static String userAlreadyExist = "User already exist";
-    public static String usernameInUse = "Username is already in use";
-    public static String unknownExceptionWhileCreatingUser = "Unknown exception while creating user";
-    public static String unknownExceptionWhileUpdatingUser = "Unknown exception while updating user";
-    public static String userCreated = "User created successfully";
-    public static String passwordChanged = "Password changed successfully";
+    public static final String userAlreadyExist = "User already exist";
+    public static final String usernameInUse = "Username is already in use";
+    public static final String unknownExceptionWhileCreatingUser = "Unknown exception while creating user";
+    public static final String unknownExceptionWhileUpdatingUser = "Unknown exception while updating user";
+    public static final String userCreated = "User created successfully";
+    public static final String passwordChanged = "Password changed successfully";
 
-    public static String userUpdated = "User updated successfully";
-    public static String userNotFound = "User not found";
-    public static String userSignedIn = "User logged in";
+    public static final String userUpdated = "User updated successfully";
+    public static final String userNotFound = "User not found";
+    public static final String userSignedIn = "User logged in";
 
-    public static String wrongUserPassword = "Wrong user password";
+    public static final String wrongUserPassword = "Wrong user password";
 
     //* Hobby related strings
-    public static String hobbyNameIsEmpty = "Hobby name is empty";
-    public static String errorOccurWhileCreatingHobby = "Error occur while creating hobby";
-    public static String hobbyDeleted = "Hobby deleted successfully";
-    public static String userAlreadyHasThisHobby = "User already has this hobby";
-    public static String hobbyAdded = "Hobby added successfully";
-    public static String hobbyNotFound = "Hobby not found";
-    public static String hobbiesFound = "Hobbies found";
-    public static String errorOccurWhileAddingHobby = "Error occur while adding hobby";
+    public static final String hobbyNameIsEmpty = "Hobby name is empty";
+    public static final String errorOccurWhileCreatingHobby = "Error occur while creating hobby";
+    public static final String hobbyDeleted = "Hobby deleted successfully";
+    public static final String userAlreadyHasThisHobby = "User already has this hobby";
+    public static final String hobbyAdded = "Hobby added successfully";
+    public static final String hobbyNotFound = "Hobby not found";
+    public static final String hobbiesFound = "Hobbies found";
+    public static final String errorOccurWhileAddingHobby = "Error occur while adding hobby";
 
     //* Post related strings
-    public static String postCreated = "Post created successfully";
-    public static String postNotFound = "Post not found";
-    public static String postDeleted = "Post deleted successfully";
-    public static String postUpdated = "Post updated successfully";
-    public static String postsFound = "Posts found";
-    public static String commentsFetched = "Post comments fetched";
-    public static String errorOccurWhileCommentingPost = "Error occur while commenting post";
-    public static String errorOccurWhileGettingComments = "Error occur while getting comments";
-    public static String errorOccurWhileGettingImages = "Error occur while getting images";
-    public static String errorWhileDislikePost = "Error while dislike post";
-    public static String errorWhileLikePost = "Error while like post";
-    public static String userPostsFetched = "User posts fetched";
-    public static String userRemoveDislike = "User remove dislike";
-    public static String userDislikedThePost = "User disliked the post";
-    public static String commentedPost = "Commented post";
-    public static String userRemoveLike = "User remove like";
-    public static String userLikedThePost = "User liked the post";
-    public static String errorOccurWhileCreatingPost = "Error occur while creating post";
+    public static final String postCreated = "Post created successfully";
+    public static final String postNotFound = "Post not found";
+    public static final String postsFound = "Posts found";
+    public static final String commentsFetched = "Post comments fetched";
+    public static final String errorOccurWhileCommentingPost = "Error occur while commenting post";
+    public static final String errorWhileDislikePost = "Error while dislike post";
+    public static final String errorWhileLikePost = "Error while like post";
+    public static final String userPostsFetched = "User posts fetched";
+    public static final String userRemoveDislike = "User remove dislike";
+    public static final String userDislikedThePost = "User disliked the post";
+    public static final String commentedPost = "Commented post";
+    public static final String userRemoveLike = "User remove like";
+    public static final String userLikedThePost = "User liked the post";
+    public static final String errorOccurWhileCreatingPost = "Error occur while creating post";
 
     //* Storage related strings
-    public static String imageUploadedSuccessfully = "Image uploaded successfully";
-    public static String imageDownloadedSuccessfully = "Image downloaded successfully";
-    public static String imageNotFound = "Image not found";
-    public static String errorOccurWhileUploadingImage = "Error occur while uploading image";
-    public static String errorWhileDownloadingImage = "Error while downloading image";
+    public static final String imageUploadedSuccessfully = "Image uploaded successfully";
+    public static final String imageNotFound = "Image not found";
+    public static final String errorOccurWhileUploadingImage = "Error occur while uploading image";
+    public static final String errorWhileDownloadingImage = "Error while downloading image";
 
     //* token
-    public static String invalidToken = "Invalid token";
+    public static final String invalidToken = "Invalid token";
 
     //* Unknown error
-    public static String unknownError = "Unknown error";
+    public static final String unknownError = "Unknown error";
 }
---
+++ hobby_garden_server/src/main/java/hobby_garden/hobby_garden_server/common/exception/FilterExceptions.java
@@ -0,0 +1,29 @@
+package hobby_garden.hobby_garden_server.common.exception;
+
+/*
+ * This class is used to handle all the exceptions that are thrown from the service layer,
+ * and then we will send the response to the client. The second throw!
+ * If you want to add new exception, you should add it here
+ */
+
+import hobby_garden.hobby_garden_server.common.constants.Strings;
+import hobby_garden.hobby_garden_server.common.exception.exceptions.*;
+
+
+public class FilterExceptions extends RuntimeException{
+     public FilterExceptions(String message) {
+        super(message);
+        switch (message){
+            case Strings.userNotFound -> throw new UserNotFoundException(message);
+            case Strings.invalidToken -> throw new InvalidTokenException(message);
+            case Strings.userAlreadyExist -> throw new UserAlreadyExist(message);
+            case Strings.userAlreadyHasThisHobby -> throw new UserAlreadyHasThisHobby(message);
+            case Strings.postNotFound -> throw new PostNotFoundException(message);
+            case Strings.hobbyNotFound -> throw new HobbyNotFoundException(message);
+            case Strings.errorOccurWhileCreatingHobby -> throw new ErrorWhileCreatingHobby(message);
+            case Strings.hobbyNameIsEmpty -> throw new BadHobbyNameFormat(message);
+            default -> throw new UnknownException(message);
+        }
+
+    }
+}
